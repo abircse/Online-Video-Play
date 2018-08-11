f65281ca33eea4dc0440fe0c2a33ce592ce56744
abircse <abircoxsbazar@gmail.com>
2018-08-12 01:56:25 +0600

    Simple Online Video Player Project

---
+++ app/src/main/java/stdtest/onlinevideoplay/Youtube.java
@@ -0,0 +1,53 @@
+package stdtest.onlinevideoplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Youtube extends YouTubeBaseActivity{
+
+    Button b;
+    private YouTubePlayerView youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+                youTubePlayer.loadVideo("a4NT5iBFuZs"); //---here use v= from url
+
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+
+        b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                youTubePlayerView.initialize("AIzaSyD5Mw1gho8xNPLi5qVneObScvNRagVz6qc",onInitializedListener);
+
+            }
+        });
+
+
+
+    }
+}
